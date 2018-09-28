d567e51d7c8053c49a4bda111e435e9e0d07d6ed
padhirishi <rishipadhi18@gmail.com>
2018-09-28 20:14:47 +0530

    MLMcommit

--- file2.R
+++ file2.R
@@ -11,6 +11,14 @@ cor(women$height, women$weight)
 
 plot(x=women$height, y=women$weight)
 abline(lm(weight ~ height, data = women), col='red')
+#abline = line of best fit, line of prediction
 names(women)
 
 fit1 = lm(weight ~ height, data = women)
+summary(fit1) #make sense of every value with T2
+
+(new1 = data.frame(height=c(65,66)))
+new1
+
+(p1=predict(fit1, newdata = new1))
+cbind(new1, p1)
