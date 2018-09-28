5b32a6036c509550d6682e4eb236264e26629f33
padhirishi <rishipadhi18@gmail.com>
2018-09-28 19:02:05 +0530

    womenadded

--- file2.R
+++ file2.R
@@ -1,1 +1,16 @@
+data(women)
 women
+names(women)
+str(women)
+
+cov(women$height, women$weight)
+
+cor(women$height, women$weight)
+
+0.995^2
+
+plot(x=women$height, y=women$weight)
+abline(lm(weight ~ height, data = women), col='red')
+names(women)
+
+fit1 = lm(weight ~ height, data = women)
